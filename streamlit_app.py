37f2fd28ffaf548a77f7761d3af16b3c08b9963a
fakedup <sokolov.nikita@gmail.com>
2022-04-20 22:01:55 +0300

    Update streamlit_app.py
    stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute('SELECT * FROM FRUIT_LOAD_LIST')
